fc55dc815339a59d3a012049646ccf10b2f0db29
0andme <yu00mi97@gmail.com>
2021-11-08 01:44:16 +0900

    feat : 사이드 바 영역의 기본 구조 작성
    - 헤더 영역
    : 마우스가 위에 있을 때만 추가 버튼 활성화

--- src/components/NotionSidebar.vue
+++ src/components/NotionSidebar.vue
@@ -1,8 +1,33 @@
+// sideBar
+// ㄴ workspaceWrap
+//   ㄴ workspaceHeader
+//   ㄴ workspaceList 
 <template>
+<div class="sideBar">
+  <!-- <div class="userInfo"></div> -->
+  <div  class="workspaceWrap">
+    <!-- 워크스페이스 헤더 -->
+    <div @mouseover="addReady=true" @mouseleave="addReady=false" class="workspaceHeader">
+      <span>WORKSPACE</span>
+      <button v-if="addReady">
+        <i class="far fa-plus-square"></i>
+      </button>
+    </div>
+
+    <!-- 워크스페이스 목록 -->
+    <ul class="workspaceList">
+    </ul>
+  </div>
+</div>
   
 </template>
 <script>
 export default {
+  data(){
+    return{
+      addReady:false
+    }
+  }
   
 }
 </script>
