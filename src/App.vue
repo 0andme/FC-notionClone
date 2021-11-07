bb837c44ea7559bfbdbc930387e5cb4cdba67d5e
0andme <yu00mi97@gmail.com>
2021-11-08 00:32:17 +0900

    feat : 기본적인 컴포넌트 생성 및 App.vue에 연결
    - NotionFrame : 워크 스페이스의 제목과 내용이 보이는 곳
    - NotionSidebar : 워크 스페이스 목록이 보이는 곳
    -NotionTopbar : 노션 사이드바 버튼과 워크스페이스 경로가 표시되는 곳

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,30 @@
 <template>
-  <h1>{{ message }}</h1>
+<NotionSidebar/>
+<!-- NotionTopbar버튼이 눌리면 사이드바 열리게 열리고 나면 버튼은 사라져야함 -->
+<!-- <NotionTopbar/> -->
+<!-- <NotionFrame/> -->
 </template>
 
 <script>
+import NotionFrame from '~/components/NotionFrame'
+import NotionSidebar from '~/components/NotionSidebar'
+import NotionTopbar from '~/components/NotionTopbar'
+
 export default {
+  components:{
+    NotionFrame,
+    NotionSidebar,
+    NotionTopbar
+  },
   data() {
     return {
-      message: 'Hello Vue SFC!'
+      
     }
+  },
+  mounted(){
+    // 데이터 가져오기
+      this.$store.dispatch('getAlldata')
+      
   }
 }
 </script>
