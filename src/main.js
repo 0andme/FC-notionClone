4da4010d7fcb4c132a19613c59ba93f6c2856ddf
0andme <yu00mi97@gmail.com>
2021-11-08 00:39:43 +0900

    feat : main.js store 사용

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
-import '~/scss/main.scss'
-import { createApp } from 'vue'
+import "~/scss/main.scss"
+import {createApp} from 'vue'
 import App from '~/App.vue'
-import store from '~/store'
+import store from "~/store";
 
 createApp(App)
   .use(store)
   .mount('#app')
+
