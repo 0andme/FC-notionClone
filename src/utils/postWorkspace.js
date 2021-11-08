5f805b683ad267544de2912b393c1c3b00faf2b5
0andme <yu00mi97@gmail.com>
2021-11-09 08:41:36 +0900

    feat : 파라미터에 부모아이디가 존재할때 api로 보내는 data파라미터 변경

--- src/utils/postWorkspace.js
+++ src/utils/postWorkspace.js
@@ -3,16 +3,21 @@ import{apiURL,headers}from'~/utils/apiInfo'
 import axios from 'axios'
 
 export async function postWorkspace({parentId,title,content}){
+  // 부모 아이디 유뮤에 따라 보내는 data 변경
+  let param
+  if(parentId===undefined){
+   param={title,content}
+  }
+  else{
+    param={parentId,title,content}
+  }
   const { data } = await axios({
-    url: `${apiURL}/${parentId}`,
+    url: apiURL,
     method: 'POST',
     headers: headers,
-    data:{
-      title,
-      content
-    }
+    data:param
   })
-  console.log('생성',data)
+  // console.log('생성',data)
   // data= { 아이디, 타이틀, 상세내용}
   return data
 }
