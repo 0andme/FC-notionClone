6d722716988d51cd5c0b7ce1fee3751c3b1533d2
0andme <yu00mi97@gmail.com>
2021-11-09 08:44:30 +0900

    feat : 사이드바 컴포넌트에 스페이스 추가 컴포넌트 및 워크스페이스 목록 컴포넌트 추가
    + 변수 이름 변경
    + 모달 창이 떴을 때도 마우스오버이벤트지속되는 것 방지 .self

--- src/components/NotionSidebar.vue
+++ src/components/NotionSidebar.vue
@@ -7,27 +7,58 @@
   <!-- <div class="userInfo"></div> -->
   <div  class="workspaceWrap">
     <!-- 워크스페이스 헤더 -->
-    <div @mouseover="addReady=true" @mouseleave="addReady=false" class="workspaceHeader">
+    <div class="workspaceHeader" @mouseover.self="isHover=true" @mouseleave.self="isHover=false" >
       <span>WORKSPACE</span>
-      <button v-if="addReady">
-        <i class="far fa-plus-square"></i>
-      </button>
+      <!-- 워크스페이스 추가 컴포넌트 -->
+      <AddWorkspace :parentId='undefined' :isHover="isHover"  @changeIsHover='changeIsHover'/>
     </div>
 
     <!-- 워크스페이스 목록 -->
-    <ul class="workspaceList">
+    <ul class="workspaceList" >
+      <template  v-for="space in this.$store.state.workspace.allSpaceList" :key="space.id">
+        <WorkspaceItem :parentId="undefined" :deepth="deepth" :space="space"/>
+      </template>
+
     </ul>
   </div>
 </div>
   
 </template>
 <script>
+import WorkspaceItem from "~/components/WorkspaceItem"
+import AddWorkspace from '~/components/AddWorkspace'
 export default {
+  components:{
+    WorkspaceItem,
+    AddWorkspace
+  },
   data(){
     return{
-      addReady:false
+      isHover:false,
+      deepth:0
     }
+  },
+  methods:{
+    changeIsHover(state){
+      this.isHover=state
+    }  
   }
   
 }
 </script>
+<style lang="scss" scoped>
+
+.sideBar{
+  width: 240px;
+
+  position: absolute;
+  left: 0;
+  top: 0;
+  bottom: 0;
+  
+  background-color: #f7f6f3;
+  .workspaceHeader{
+    
+  }
+}
+</style>
