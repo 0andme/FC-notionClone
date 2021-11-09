3c0feb54eb9435e93a4b63c21cc798c15dd977b5
0andme <yu00mi97@gmail.com>
2021-11-09 09:17:26 +0900

    feat : 워크스페이스 아이템 컴포넌트 생성 및 초안 작성
    - 토글버튼
    - 워크스페이스 타이틀
    - 수정 삭제컴포넌트 연결
    - 자식워크스페이스 있을 경우 재귀
    - 자식 워크스페이스 없을 경우의 기본 메시지 설정

---
+++ src/components/WorkspaceItem.vue
@@ -0,0 +1,70 @@
+<template>
+
+<div @mouseover.self="isHover=true" @mouseleave.self="isHover=false">
+  <!-- 토글 버튼 -->
+  <button @click="isOpen=!isOpen">
+    <i v-if="isOpen" class="fas fa-caret-down"></i>
+    <i v-if="!isOpen" class="fas fa-caret-right"></i>
+  </button>
+  <!-- 워크 스페이스 타이틀 -->
+  <span>{{space.title}}</span>
+  <!-- 워크스페이스 수정 삭제 컴포넌트 -->
+  <ControlSpace :space="space" :parentId='parentId' :isHover="isHover"  @changeIsHover='changeIsHover'/>
+  <!-- 워크스페이스 추가 컴포넌트 -->
+  <template  v-if="deepth<3"> 
+    <AddWorkspace  :parentId='space.id' :isHover="isHover"  @changeIsHover='changeIsHover'/>
+  </template>
+
+  <!-- 토글 버튼이 열렸을 경우 -->
+  <template v-if="isOpen">
+    <!-- 자식 컴포넌트 있을 경우 -->
+    <div v-if="space.children" >
+      <template v-for="child in space.children" :key="child.id">
+        <WorkspaceItem  :deepth="deepth+1" :parentId="space.id" :space="child" />
+      </template>
+    </div>
+    <!-- 자식 컴포넌트 없을 경우 -->
+    <div v-else>하위 페이지가 없습니다</div>
+  </template>
+
+</div>
+  
+</template>
+<script>
+import AddWorkspace from "~/components/AddWorkspace"
+import ControlSpace from "~/components/ControlSpace"
+
+
+export default {
+  components:{
+    AddWorkspace,
+    ControlSpace,
+  },
+  props:{
+    space:{
+      type:Object,
+    },
+    parentId:{
+      type:String,
+      default:undefined
+    },
+    deepth:{
+      type:Number,
+      requried:true
+    }
+  },
+  data(){
+    return{
+      isHover:false,
+      isOpen:false
+    }
+  },
+  methods:{
+    changeIsHover(state){
+      this.isHover=state
+    },
+  }
+  
+  
+}
+</script>
